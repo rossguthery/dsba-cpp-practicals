6d1af1563b8db80f60e76bd246b6aff39a33f9bd
rossguthery <rosswguthery@gmail.com>
2021-01-19 00:10:10 +0100

    Complete ex7.

--- practical-2/ex7.cpp
+++ practical-2/ex7.cpp
@@ -2,7 +2,10 @@
 // "a." It uses two different definitions to do so. Using two global variables, it also displays to the user the
 // number of recursive calls needed by each definition to perform said computations.
 
-// EXPLAIN RELATIONSHIP BETWEEN EXPONENT VALUE AND CALLS.
+// For the first definition, the number of recursive calls is always equal to the power "n" plus one. For example, ten
+// squared requires three recursive calls. For the second definition, the number of recursive calls exhibits the
+// behavior of a step function. That is, for n=1, it requires 3 calls; for n=[2,3], 7; for n=[4,7], 15; and so on and so
+// forth. The length of each step increases as "n" increases.
 
 #include <iostream>
 using namespace std;
@@ -10,40 +13,33 @@ int counter1 = 0;
 int counter2 = 0;
 
 int def_one (int a, int n) {
-    counter1 += 1;
-    int i = 0;
+    counter1++;
     if (n == 0)
         return 1;
     else
-        while (i < n) {
-            a =
-        }
-
-        return a*pow(a, n-1);
+        return (a*def_one(a, n-1));
 }
 
 int def_two (int a, int n) {
-    counter2 += 1;
+    counter2++;
     if (n == 0)
         return 1;
     else if (n%2 == 0)
-        return pow(a, n/2)*pow(a, n/2);
+        return (def_two(a, n/2)*def_two(a, n/2));
     else
-        return a*(pow(a, n/2)*pow(a, n/2));
+        return (a*def_two(a, n/2)*def_two(a, n/2));
 }
 
 int main () {
     int a, n;
     cout << "Integer: ";
     cin >> a;
-    cout << "Non-Negative Integer: ";
+    cout << "Non-negative integer: ";
     cin >> n;
-    if (n < 0)
-        cout << "Please re-enter a non-negative integer: ";
-        cin >> n;
     cout << "————————————————   " << a << " to the power of " << n << "   ————————————————" << endl;
-    cout << "First Definition: " << def_one(a, n) << "Recursive Calls: " << counter1 << endl;
-    cout << "Second Definition: " << def_two(a, n) << "Recursive Calls: " << counter2 << endl;
+    cout << "First Definition: " << def_one(a, n) << "   Recursive Calls: " << counter1 << endl;
+    cout << "Second Definition: " << def_two(a, n) << "   Recursive Calls: " << counter2 << endl;
     return 0;
 }
+
 
